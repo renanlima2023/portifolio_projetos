d83fa2b32f2c9e85f7d041c144d06af8b16557c7
Renan <147100834+renanlima2023@users.noreply.github.com>
2025-07-29 10:21:53 -0300

    fix: Final e definitivo ajuste de base URL para imagens no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "./",
+  base: "https://renanlima2023.github.io/portifolio_projetos/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
