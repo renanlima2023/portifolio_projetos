8f8bd4f3c9bff5dc883e26b6225e56748783091f
Renan <147100834+renanlima2023@users.noreply.github.com>
2025-07-29 09:26:08 -0300

    fix: Ajuste final do base do Vite para deploy no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "./",
+  base: "/portifolio_projetos/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
